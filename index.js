53c283b9b3e6c6e61778ae342c44bdc17ed46ed4
MarkXtenda <mark.i2p.1998@gmail.com>
2022-02-24 12:40:34 +1100

    Pass

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
-// Write your solution in this file!
+let employee = {
+    name: "Mark",
+    streetAdress: "123"
+}
+
+function updateEmployeeWithKeyAndValue(employee, key, value) {
+    return {
+        ...employee,
+        [key]: value
+    }
+}
+
+function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value) {
+    employee[key] = value
+    return employee
+}
+
+function deleteFromEmployeeByKey(employee, key) {
+    const employeeCopy = {
+        ...employee
+    }
+    delete employeeCopy[key]
+    return employeeCopy
+}
+
+function destructivelyDeleteFromEmployeeByKey(employee, key) {
+    delete employee[key]
+    return employee
+}
